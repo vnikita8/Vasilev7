1b5fa6fe4213e270f812bb9a7fe8a57858a16dcf
vnikita <vnikita123456@gmail.com>
2021-11-07 16:28:49 +0700

    Название кнопки -_-

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -480,7 +480,7 @@ namespace Vasilev7
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(158, 23);
             this.button5.TabIndex = 16;
-            this.button5.Text = "Вывести большее";
+            this.button5.Text = "НОД";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
